a3b2b9d95a47ce9e4877b889d3c18003b5af1afe
aakansha1309 <tidkeaakansha@gmail.com>
2024-02-06 21:55:56 +0530

    ToDoApp using express

---
+++ controllers/getTodo.js
@@ -0,0 +1,59 @@
+//import the model
+const Todo = require("../models/Todo");
+
+//define route handler
+
+exports.getTodo = async(req,res) => {
+    try {
+            // fetch all todo items from database
+            const todos = await Todo.find({});
+
+            // response update
+        res.status(200)
+        .json({
+            success:true,
+            data:todos,
+            message:"Entire Todo Data is fetched",
+        })  
+    }
+    catch(err) {
+      console.error(err);
+      res.status(500)
+      .json({
+        success:false,
+        error:err.message,
+        message:"Server Error",
+      });
+    }
+}
+
+exports.getTodoById = async(req,res) => {
+    try {
+       // extract todo items basis on id
+       const id = req.params.id;
+       const todo = await Todo.findById({_id: id})
+
+       // error in given data or data not found
+       if(!todo) {
+        return res.status(404).json({
+            success:false,
+            message:"No data Found with given Id",
+        })
+       }
+       //data for given id found
+       res.status(200).json({
+        success:true,
+        data:todo,
+        message: `ToDo ${id} data successfully fetched`,
+       })
+}
+catch(err) {
+    console.error(err);
+    res.status(500)
+    .json({
+      success:false,
+      error:err.message,
+      message:"Server Error",
+    });
+}
+}
